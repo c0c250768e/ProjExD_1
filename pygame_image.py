69f030e6a7770285a03bc39394811ddd18cc6c83
Murai Kenshin <c0c250768e@edu.teu.ac.jp>
2026-04-14 15:43:32 +0900

    練習1234

---
+++ pygame_image.py
@@ -0,0 +1,31 @@
+import os
+import sys
+import pygame as pg
+
+os.chdir(os.path.dirname(os.path.abspath(__file__)))
+
+
+def main():
+    pg.display.set_caption("はばたけ！こうかとん")
+    screen = pg.display.set_mode((800, 600))
+    clock  = pg.time.Clock()
+    bg_img = pg.image.load("fig/pg_bg.jpg")#練習1
+    kk_img = pg.image.load("fig/3.png")#練習3
+    kk_img = pg.transform.flip(kk_img, True,False)#5
+    tmr = 0
+    while True:
+        for event in pg.event.get():
+            if event.type == pg.QUIT: return
+
+        screen.blit(bg_img, [0, 0])#練習2
+        screen.blit(kk_img, [300, 200])#練習4
+        pg.display.update()
+        tmr += 1        
+        clock.tick(10)
+
+
+if __name__ == "__main__":
+    pg.init()
+    main()
+    pg.quit()
+    sys.exit()
