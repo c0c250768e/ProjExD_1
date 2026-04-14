966e572b6584227dd4647935281380b1cad44721
Murai Kenshin <c0c250768e@edu.teu.ac.jp>
2026-04-14 17:22:08 +0900

    演習2

--- pygame_image.py
+++ pygame_image.py
@@ -21,16 +21,16 @@ def main():
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT: return
-
+        a = kk_rct.move_ip
         key_lst = pg.key.get_pressed()
         if key_lst[pg.K_UP]:
-            kk_rct.move_ip((0, -1)) 
+            a((0, -1)) 
         if key_lst[pg.K_DOWN]:
-            kk_rct.move_ip((0, 1))
+            a((0, 1))
         if key_lst[pg.K_LEFT]:
-            kk_rct.move_ip((-1, 0))       
+            a((-1, 0))       
         if key_lst[pg.K_RIGHT]:
-            kk_rct.move_ip((2, 0))
+            a((2, 0))
         x = tmr%3200#6
         screen.blit(bg_img, [-x, 0])#練習2,5
         screen.blit(bg_img2, [-x+1600, 0])#練習7
