a7a9ea7f013a61dfdd93ca5bbecd7551153e9021
Murai Kenshin <c0c250768e@edu.teu.ac.jp>
2026-04-14 17:40:32 +0900

    名前変更

---
+++ flying_kokaton.py
@@ -0,0 +1,49 @@
+import os
+import sys
+import pygame as pg
+
+os.chdir(os.path.dirname(os.path.abspath(__file__)))
+
+
+def main():
+    pg.display.set_caption("はばたけ！こうかとん")
+    screen = pg.display.set_mode((800, 600))
+    clock  = pg.time.Clock()
+    bg_img = pg.image.load("fig/pg_bg.jpg")#練習1
+    bg_img2 = pg.transform.flip(bg_img, True,False)#7
+    kk_img = pg.image.load("fig/3.png")#練習3
+    kk_img = pg.transform.flip(kk_img, True,False)#5
+    kk_rct = kk_img.get_rect()
+    kk_rct.center = 300, 200
+    screen.blit(kk_img, kk_rct)
+
+    tmr = 0
+    while True:
+        for event in pg.event.get():
+            if event.type == pg.QUIT: return
+        a = kk_rct.move_ip
+        key_lst = pg.key.get_pressed()
+        if key_lst[pg.K_UP]:
+            a((0, -1)) 
+        if key_lst[pg.K_DOWN]:
+            a((0, 1))
+        if key_lst[pg.K_LEFT]:
+            a((-1, 0))       
+        if key_lst[pg.K_RIGHT]:
+            a((2, 0))
+        x = tmr%3200#6
+        screen.blit(bg_img, [-x, 0])#練習2,5
+        screen.blit(bg_img2, [-x+1600, 0])#練習7
+        screen.blit(bg_img, [-x+3200, 0])#練習7
+        screen.blit(kk_img, kk_rct)
+        kk_rct.move_ip((-1,0))
+        pg.display.update()
+        tmr += 1        
+        clock.tick(200)#6
+
+
+if __name__ == "__main__":
+    pg.init()
+    main()
+    pg.quit()
+    sys.exit()
